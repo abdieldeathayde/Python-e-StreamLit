6966559a98d00dccedb424af1abf3fe86207f432
Abdiel de Athayde <abdieldeathayde67@gmail.com>
2026-05-07 21:30:11 -0300

    criacao de sites com streamlit

---
+++ auxiliar.py
@@ -0,0 +1,17 @@
+# listas
+lista_nova = []
+nomes = ['João', 'Maria', 'Pedro']
+print(nomes[0])
+
+nomes.append('Ana')
+print(nomes)
+
+# dicionario
+idades = {'João': 30, 'Maria': 25, 'Pedro': 20}
+
+
+cookies = {
+    "fb_id_usuario": "123456789",
+    "email": "joao@example.com",
+    "lista_mensagens": []
+}
---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from openai import OpenAI
+
+modelo_ia = OpenAI(api_key="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")
+
+st.write("# Chatbot com IA")
+
+if not "lista_mensagens" in st.session_state:
+    st.session_state["lista_mensagens"] = []
+#st.session_state
+
+
+
+
+texto_usuario = st.chat_input("Digite sua mensagem aqui...")
+
+for mensagen in st.session_state["lista_mensagens"]:
+    role = mensagen["role"]
+    content = mensagen["content"]
+    st.chat_message(role).write(content)
+   
+
+if texto_usuario:
+    st.chat_message("user").write(texto_usuario)
+    mensagem_usuario = {"role": "user", "content": texto_usuario}
+    st.session_state["lista_mensagens"].append(mensagem_usuario)
+
+    #IA respondeu
+    resposta_ia =  modelo_ia.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=st.session_state["lista_mensagens"]
+    )
+
+    texto_resposta_ia = resposta_ia.choices[0].message.content
+    resposta_ia = resposta_ia.choices[0].message
+
+    st.chat_message("assistant").write(resposta_ia)
+    mensagem_ia = {"role": "assistant", "content": resposta_ia}
+    st.session_state["lista_mensagens"].append(mensagem_ia)
