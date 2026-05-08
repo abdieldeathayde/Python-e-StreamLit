697f117d5c2fd33dd55bb5c29354153dea5adf58
Abdiel de Athayde <abdieldeathayde67@gmail.com>
2026-05-08 13:31:10 -0300

    atualizando projeto

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
 import streamlit as st
-from openai import OpenAI
-
-modelo_ia = OpenAI(api_key=os.getenv("OPENAI_API_KEY")
-
 
 st.write("# Chatbot com IA")
 
@@ -19,23 +14,20 @@ texto_usuario = st.chat_input("Digite sua mensagem aqui...")
 for mensagen in st.session_state["lista_mensagens"]:
     role = mensagen["role"]
     content = mensagen["content"]
-    st.chat_message(role).write(content)
-   
+    if mensagen["role"] == "user":
+        st.chat_message("user").write(mensagen["content"])
+    else:
+        st.chat_message("assistant").write(mensagen["content"])
 
 if texto_usuario:
     st.chat_message("user").write(texto_usuario)
     mensagem_usuario = {"role": "user", "content": texto_usuario}
     st.session_state["lista_mensagens"].append(mensagem_usuario)
 
-    #IA respondeu
-    resposta_ia =  modelo_ia.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=st.session_state["lista_mensagens"]
-    )
-
-    texto_resposta_ia = resposta_ia.choices[0].message.content
-    resposta_ia = resposta_ia.choices[0].message
+    resposta_ia = "Você perguntou: " + texto_usuario
 
     st.chat_message("assistant").write(resposta_ia)
     mensagem_ia = {"role": "assistant", "content": resposta_ia}
     st.session_state["lista_mensagens"].append(mensagem_ia)
+
+print(st.session_state["lista_mensagens"])
